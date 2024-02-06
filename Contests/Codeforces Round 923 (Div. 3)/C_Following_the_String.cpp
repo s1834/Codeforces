6c36ead3c22340b275121ab918c0f1603587c5d8
s1834 <s18324@protonmail.com>
2024-02-06 22:30:04 +0530

    Wrong answer on test 4

---
+++ Contests/Codeforces Round 923 (Div. 3)/C_Following_the_String.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	cin >> t;
+ 
+	while (t--) {
+		int n;
+		cin >> n;
+ 
+		vector<int> a(n);
+		for(int i = 0; i < n; i++) {
+			cin >> a[i];
+		}
+ 
+		map<int, vector<char>> mp;
+		string s = "";
+		for(int i = 0; i < n; i++) {
+			if(mp[a[i]].size() == 0) {
+				mp[a[i]].push_back('a');
+				s += 'a';
+			} else {
+				mp[a[i]].push_back(mp[a[i]][mp[a[i]].size() - 1] + 1);
+				s += mp[a[i]][mp[a[i]].size() - 1] + 1;
+			}
+		}
+		cout << s << endl;
+	}
+ 
+	return 0;
+}
