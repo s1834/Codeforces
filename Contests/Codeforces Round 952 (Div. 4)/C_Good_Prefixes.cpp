2c079e55d7e66e40a29aa98454f333c8949151e0
s1834 <s18324@protonmail.com>
2024-06-11 21:49:16 +0530

    Wrong Answer

--- Contests/Codeforces Round 952 (Div. 4)/C_Good_Prefixes.cpp
+++ Contests/Codeforces Round 952 (Div. 4)/C_Good_Prefixes.cpp
@@ -9,18 +9,20 @@ int main() {
 		int n;
 		cin >> n;
 
-		long long int a[n], prefixSum[n];
-		prefixSum[0] = 0;
+		long long int a[n], prefixSum[n][2];
+		prefixSum[0][0] = 0;
+		prefixSum[0][1] = 0;
 		for (int i = 0; i < n; i++) {
 			cin >> a[i];
 			if(i != 0) {
-				prefixSum[i] = prefixSum[i - 1] + a[i - 1];
+				prefixSum[i][0] = prefixSum[i - 1][0] + a[i - 1];
+				prefixSum[i][1] = prefixSum[i - 1][1] > a[i] ? prefixSum[i - 1][1] : a[i];
 			}
 		}
 
 		int count = 0;
 		for (int i = 0; i < n; i++) {
-			if(prefixSum[i] == a[i] || (i != 0 && prefixSum[i - 1] + a[i] == a[i - 1])) {
+			if(prefixSum[i][0] - prefixSum[i][1] + a[i] == prefixSum[i][1]) {
 				count++;
 			}
 		}
