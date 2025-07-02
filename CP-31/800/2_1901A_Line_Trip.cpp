fd8e4a317aa0951fefba9c2221bb756770e7588c
s1834 <s18324@protonmail.com>
2025-07-02 15:26:16 +0530

    Accepted

--- CP-31/800/2_1901A_Line_Trip.cpp
+++ CP-31/800/2_1901A_Line_Trip.cpp
@@ -9,16 +9,27 @@ int main() {
         int n, x;
         cin >> n >> x;
 
-        int arr[n], mx = 0;
-        for(int i = 0; i < n; i++) {
-            cin >> arr[i];
-            if(i > 0 && arr[i] - arr[i - 1] > mx) mx = arr[i] - arr[i - 1];
+        int arr[n + 2];
+        arr[0] = 0;
+        for(int i = 0; i < n; i++) cin >> arr[i + 1];
+
+        int mx = 0;
+        arr[n + 1] = x;
+        n += 2;
+        for(int i = 1; i < n; i++) {
+            if(i == n - 1) mx = max(mx, 2 * (arr[i] - arr[i - 1]));
+            else mx = max(mx, arr[i] - arr[i - 1]);
         }
-        if(n == 1) cout << max(arr[0], 2 * (x - arr[0])) << endl;
-        else cout << max(mx, 2 * (x - arr[n - 1])) << endl;
+        cout << mx << endl;
     }
 
     return 0;
 }
 
-/*  */
+/* 
+1. add 0 at the start and x at the end of the array
+2. find the maximum distance between two consecutive elements in the array
+3. if the last element is not the maximum, then double the distance between the last two elements to account for the circular nature of the problem
+
+dont make silly mistaked, read the problem and code carefully
+*/
