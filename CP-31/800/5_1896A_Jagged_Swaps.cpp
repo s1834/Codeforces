5261a46ebd1a31929612bf9594270ab34c6686f6
s1834 <s18324@protonmail.com>
2025-07-04 01:18:08 +0530

    Accepted

--- CP-31/800/5_1896A_Jagged_Swaps.cpp
+++ CP-31/800/5_1896A_Jagged_Swaps.cpp
@@ -10,16 +10,16 @@ int main() {
         cin >> n;
 
         vector<int> a(n);
-        int flag = true;
-        for(int i = 0; i < n; i++) {
-            cin >> a[i];
-            if(a[i] != i && a[i] != i + 1 && a[i] != i + 2) flag = false;
-        }
-        if(flag) cout << "YES\n";
+        for(int i = 0; i < n; i++)  cin >> a[i];
+        if(a[0] == 1) cout << "YES\n";
         else cout << "NO\n";
     }
     return 0;
 }
 
 /*
+if the first element is yes then the answer is yes, otherwise no.
+as we can only change from index 2 to n - 1
+
+before what i was doing wrong is i was cheking if it fits in +1 or -1 or currect position as i did a wrong dry run and thought that 2 to n - 1 is to confuse
 */
