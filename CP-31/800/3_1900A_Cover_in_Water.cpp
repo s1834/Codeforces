0e52224b60b9d35f494dfcadec1cb4608955d37a
s1834 <s18324@protonmail.com>
2025-07-03 22:34:42 +0530

    Accepted

--- CP-31/800/3_1900A_Cover_in_Water.cpp
+++ CP-31/800/3_1900A_Cover_in_Water.cpp
@@ -12,26 +12,24 @@ int main() {
         cin >> s;
 
         int three = 0, single = 0;
-        for(int i = 1; i < n - 1; i++) {
+        for(int i = 0; i < n; i++) {
             if(s[i] == '.') {
-                if(s[i - 1] == '.' && s[i + 1] == 
-                '.') {
+                if(i + 1 < n && s[i + 1] == '.' && i + 2 < n && s[i + 2] == '.') {
                     three++;
                     break;
                 } else single++;
             }
         }
         if(three) cout << 2 << endl;
-        else {
-            if(s[0] == '.') single++;
-            if(s[n - 1] == '.') single++;
-            cout << single << endl;
-        }
+        else cout << single << endl;
+        
     }
-    
     return 0;
 }
 
 /* 
+1. if three consecutive dots are found, print 2
+2. else print the number of single dots
 
+dont overcomplicate by adding multiple if conditions, try to keep it simple
 */
